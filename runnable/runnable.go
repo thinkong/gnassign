cf48cda1dc3973883940be3201964cc982709019
Kyuhun Lee <thinkong@hotmail.com>
2017-05-18 04:23:15 +0900

    created runnable

--- runnable/runnable.go
+++ runnable/runnable.go
@@ -3,6 +3,8 @@ package main
 import (
 	"github.com/thinkong/gnassign"
 	"log"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func main() {
@@ -12,4 +14,15 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
+	r := gin.Default()
+	r.GET("/download_image", func(c *gin.Context) {
+		requrl := c.Query("url")
+		matched := blackList.SearchMatch(requrl)
+		if matched {
+			c.Status(http.StatusForbidden)
+		} else {
+			c.Status(http.StatusOK)
+		}
+	})
+	r.Run(":8080")
 }
